1fdf54a757854adc36320811892161d0eb7eabe3
saytkhanov <81220037+saytkhanov@users.noreply.github.com>
2021-08-05 19:56:08 +0300

    made a conclusion of the data

---
+++ src/Weather.jsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from "react";
+import { getApiUrl } from "./utils";
+
+function Weather({ city, key }) {
+  const [weather, setWeather] = useState({});
+  const temp = Math.floor(weather.main?.temp - 273.15);
+  const feels = Math.floor(weather.main?.feels_like - 273.15);
+  console.log(weather);
+
+  useEffect(() => {
+    fetch(getApiUrl(city))
+      .then((res) => res.json())
+      .then((json) => {
+        setWeather(json);
+      });
+  }, [city]);
+
+  if (!Object.keys(weather).length) {
+    return <div>loading of weather...</div>;
+  }
+
+  return (
+    <div className="container-card" key={key}>
+      <div className="header">
+        <div>
+          <b>{weather.name}</b>, {weather.sys.country}{" "}
+        </div>
+      </div>
+      <div className="weather-icon">
+        <div>
+          <img
+            src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+            alt="icon"
+          />
+        </div>
+        <div>
+          <h1>{temp} &deg;C</h1>
+        </div>
+      </div>
+      <div className="other-info">
+        <p>Feels like - {feels}&deg;C</p>
+      </div>
+      <div className="windAndPressure">
+        <div className="wind">
+          <p>{weather.wind?.speed}m/s SSE</p>
+        </div>
+        <div>
+          <p>{weather.main?.pressure}hPa</p>
+        </div>
+      </div>
+      <div>
+        <div>
+          <p>Humidity: {weather.main?.humidity}%</p>
+        </div>
+        <div>
+          <p>Visibility: {weather.visibility / 1000}km</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Weather;
